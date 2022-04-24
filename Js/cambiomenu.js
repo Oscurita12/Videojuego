72a0a27390e2598a403d8a374e780bc0e8fcff93
Juan <juanpabloramirezflorez@gmail.com>
2022-04-23 19:46:19 -0500

    no se hizo

--- Js/cambiomenu.js
+++ Js/cambiomenu.js
@@ -1,14 +1,38 @@
-let menu=document.getElementById("menu")
+/*let menu=document.getElementById("menu")
 
 window.addEventListener("scroll",function(){
 
     if (this.document.documentElement.scrollTop<=200 ) {
+        menu.classList.add("navbar-light")
 
         menu.classList.add("menu")
         menu.classList.remove("menu2")
     }
     else {
+        menu.classList.add("navbar-dark")
+
+        document.getElementById("menu").style.color = "#cc3fb2";        
         menu.classList.add("menu2")
-        menu.classList.remove("menu")
+        menu.classList.remove("menu")   
     }
-})
+})*/
+
+/*window.onscroll = function() {
+    scrollFunction()
+  };
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      document.getElementById("menu").style.height = "75px";
+      document.getElementById("menu").style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+      document.getElementById("menu").style.color = "#000";
+      document.getElementById("menu").style.line - height;
+      "37.5px";
+    } else {
+      document.getElementById("menu").style.height = "100px";
+      document.getElementById("menu").style.backgroundColor = "rgba(255, 255, 255, 0.1)";
+      document.getElementById("menu").style.color = "#fff";
+      document.getElementById("menu").style.line - height;
+      "50px";
+    }
+  }*/
